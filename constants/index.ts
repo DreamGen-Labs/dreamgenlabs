cf52c0fff2df0b12e29b5eca8449c07819f11c89
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-10-01 20:13:54 +0530

    Add constants for navigation list, why us details, services, team members, about us, and projects

---
+++ constants/index.ts
@@ -0,0 +1,178 @@
+import { title } from "process";
+
+export const nav_list = [
+  { name: "Home", href: "/" },
+  { name: "Services", href: "/services" },
+  { name: "Projects", href: "/projects" },
+  { name: "Careers", href: "/careers" },
+  { name: "Blog", href: "/blog" },
+  { name: "About", href: "/about" },
+];
+
+export const whyUsDetails = [
+  {
+    image: "/why-us-icon/First Place Ribbon.png",
+    title: "Expertise",
+    description:
+      "At DreamGen Labs we comprise experienced experts in web development and digital marketing, bringing years of industry knowledge to every project.",
+  },
+  {
+    image: "/why-us-icon/rocket.png",
+    title: "Innovative Solutions",
+    description:
+      "We leverage the latest technologies and trends to create modern, user-friendly websites and effective digital marketing strategies.",
+  },
+  {
+    image: "/why-us-icon/Hand Holding US Dollar_.png",
+    title: "Modest Pricing",
+    description:
+      "We deliver exceptional value with affordable high-quality web development and digital marketing services, maintaining transparent pricing.",
+  },
+  {
+    image: "/why-us-icon/Heart Puzzle.png",
+    title: "Unified Services",
+    description:
+      "We offer a seamless blend of IT solutions under one roof. Experience the convenience and reliability of a single point of contact for all your IT requirements with DreamGen Labs.",
+  },
+  {
+    image: "/why-us-icon/Helping Hands.png",
+    title: "Client-Focused",
+    description:
+      "Our priority is your satisfaction. We work closely with you throughout the process to ensure your vision is realized and your expectations are exceeded.",
+  },
+  {
+    image: "/why-us-icon/Customer Support.png",
+    title: "Support Services",
+    description:
+      "We provide efficient support services tailored to your needs, ensuring smooth system operations through troubleshooting, maintenance, and upgrades.",
+  },
+];
+
+export const services = [
+  {
+    image: "/services-icon/robot-with-megaphone-directors-chair 1.png",
+    title: "Social Media Management",
+    description:
+      "Our social media management service involves expertly handling all aspects of your online presence. From content creation to audience engagement and performance monitoring, we ensure your brand shines across platforms.",
+  },
+  {
+    image: "/services-icon/robot-with-laptop-computer-mouse 1.png",
+    title: "Software Development & Maintenance",
+    description:
+      "We excel at crafting visually stunning and intuitive interfaces while utilize ing top-notch UI/UX design principles, ensuring seamless user experiences that leave a lasting impression.",
+  },
+  {
+    image: "/services-icon/Robot with a tablet.png",
+    title: "Commercial Website Development",
+    description:
+      "Our Commercial Website Development service offers tailored solutions for businesses seeking a strong online presence, driving traffic and maximizing conversions.",
+  },
+  {
+    image: "/services-icon/robot-with-digital-tablet 1.png",
+    title: "Mobile Application Development",
+    description:
+      "Our Commercial Website Development service offers tailored solutions for businesses seeking a strong online presence, driving traffic and maximizing conversions.",
+  },
+  {
+    image: "/services-icon/Robot writing with a pen.png",
+    title: "Front End Web Development",
+    description:
+      "We excel at crafting visually stunning and intuitive interfaces while utilize ing top-notch UI/UX design principles, ensuring seamless user experiences that leave a lasting impression.",
+  },
+  {
+    image: "/services-icon/robot-with-helmet-clipboard.png",
+    title: "Search Engine Optimization ",
+    description:
+      "Our Search Engine Optimization (SEO) service boosts your online visibility, driving more organic traffic to your website and improving your search engine rankings.",
+  },
+];
+
+export const teamMembers = [
+  {
+    image: "/img/team/mitila.png",
+    name: "J.M Thilochana",
+    designation: "Founder & CEO",
+    link: {
+      facebook: "https://www.facebook.com/mithila.thilochana?mibextid=LQQJ4d",
+      instragram: "",
+      linkedin: "https://www.linkedin.com/in/mithila-thilochana-b364362a0",
+      github: "https://github.com/It22899224J",
+    },
+  },
+  {
+    image: '/img/team/lahiru.png',
+    name: "W.D.L Udayakumara",
+    designation: "Co-Founder & CTO",
+    link: {
+      facebook: "https://www.facebook.com/lahiruudaya.kumara.5/",
+      instragram: "https://www.instagram.com/lahiru3d/",
+      linkedin: "https://www.linkedin.com/in/lahiru-udayakumara-716a4b235/",
+      github: "https://github.com/lahiruudayakumara",
+    },
+  },
+  {
+    image: '/img/team/yohan.png',
+    name: "W.D.Y.C Werrapperuma",
+    designation: "Chief Financial Officer",
+    link: {
+      facebook:
+        "https://www.facebook.com/share/q3qJK1C4vy4BMwk5/?mibextid=qi2Omg",
+      instragram: "https://www.instagram.com/christmith/",
+      linkedin: "https://www.linkedin.com/in/yohanchristmith/",
+      github: "https://github.com/Christmith",
+    },
+  },
+  {
+    image: "/img/team/sithum.png",
+    name: "S.M.A.S.N Senanayake",
+    designation: "Chief Operating Officer",
+    link: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100012966885512&mibextid=ZbWKwL",
+      instragram: "https://www.instagram.com/_this.is.sithum_/",
+      linkedin:"https://www.linkedin.com/in/sithum-senanayake-b11249239/",
+      github: "https://github.com/sithum-senanayake",
+    },
+  },
+  {
+    image: "/img/team/nimsara.png",
+    name: "N.A.D Nimsara",
+    designation: "Chief Marketing Officer",
+    link: {
+      facebook: "https://m.facebook.com/duvindu.nimsara",
+      instragram: "https://www.instagram.com/duv_indu/",
+      linkedin: "https://www.linkedin.com/in/duvindu-nimsara/",
+      github: "https://github.com/duvindunimsara",
+    },
+  }
+];
+
+export const about_us = [
+  {
+    image: "/img/about us 1.svg",
+    title: "Values",
+    description:
+      "Innovation, Integrity, Collaboration, Empowerment, and Sustainability guide our journey, shaping our approach to technology and business.",
+  },
+  {
+    image: "/img/about us 2.svg",
+    title: "Vision",
+    description:
+      "Empowering businesses with innovative, modern IT solutions for sustainable growth, we provide cutting-edge technology  to ensure long-term success.",
+  },
+  {
+    image: "/img/about us 3.svg",
+    title: "Goal",
+    description:
+      "Pioneering the digital frontier, weaving a tapestry of global excellence, we empower businesses with innovative, modern IT solutions for sustainable growth.",
+  },
+];
+
+export const projects = [
+  { title: 'Harvest Master', description: 'Description of Project Alpha', image: '/img/harvestmasterimg.png' },
+  { title: 'DormLink', description: 'Description of Project Beta', image: '/img/dormlinkimg.png' },
+  { title: 'Tour Buddy', description: 'Description of Project Alpha', image: '/img/Tourbuddyimg.png' },
+  { title: 'Crop Care', description: 'Description of Project Beta', image: '/img/cropcareimg.png' },
+  { title: 'Pet Care', description: 'Description of Project Alpha', image: '/img/petcareimg.png' },
+  { title: 'Coffe Bean', description: 'Description of Project Beta', image: '/img/coffeebeanimg.png' },
+];
