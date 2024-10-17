c05444c65aa3b8685baaa920a256d7fbcf329998
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-10-17 22:37:08 +0530

    bug fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,7 +7,7 @@ export default function NotFoundPage() {
     <>
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <img src="./img_404.jpg" alt="404" width={400} height={400} />
+          <img src="./img_404.jpg" alt="404" className="w-96 h-auto"/>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
           <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
